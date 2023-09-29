7c3653c98514c1b8f143e3033b7eca093461c5a1
Muhammad Nawaz <sardarnawaz122@gmail.com>
2023-09-29 22:07:59 +0500

    php tutorial for beginner

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+//  Arrays
+// $array=array(1,2,3,4,5,);
+// $array2['pakistan']="Urdu";
+// $array2['India']="Hindi";
+// $array2['bangladash']="Bangladash";
+
+// $array2=array('pakistan'=> 'Urdu','India'=> 'Hindi','Bangladash'=> 'Bangladash','Nepal'=> 'Bangladash',);
+ 
+
+// print_r($array2);
+// echo sizeof($array2);
+?>
