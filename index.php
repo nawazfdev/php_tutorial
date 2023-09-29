74ff5f6b7dc516cbe9a99c540827726011fa3185
Muhammad Nawaz <sardarnawaz122@gmail.com>
2023-09-29 22:35:52 +0500

    forloop, while loop, do while loop, foreach loop is added

--- index.php
+++ index.php
@@ -41,4 +41,28 @@
 
 
 // }
+// while loop
+// $x = 1;
+
+// // while($x <= 5) {
+// //   echo "The number is: $x <br>";
+// //   $x++;
+// // }
+// for($x;$x<=5;$x++){
+//     echo "The number is: $x <br>";
+
+// }
+// do while loop
+// $x=1;
+// do {
+//     echo "The number is: $x <br>";
+//     $x++;
+// } while ($x <= 4);
+
+// foreach loop
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
 ?>
